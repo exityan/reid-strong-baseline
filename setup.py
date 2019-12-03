1c86278e6225619f036f91013303359023d56a17
exityan <exityan@gmail.com>
2019-12-03 12:36:50 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="reid-strong-baseline",
+    version="0.1.0",
+    description="Bag of Tricks and A Strong Baseline for Deep Person Re-identification",
+    packages=find_packages(),
+)
